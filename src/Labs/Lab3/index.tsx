f4528afecf0d04f0426af94823e6b54abbffea06
Quang Gia Nguyen <91767053+LordChunky@users.noreply.github.com>
2024-10-10 22:02:47 -0400

    worked up until "2.3.2 Implied returns" in A3

---
+++ src/Labs/Lab3/index.tsx
@@ -0,0 +1,29 @@
+import ArrowFunctions from "./ArrowFunctions";
+import BooleanVariables from "./BooleanVariables";
+import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
+import ConditionalOutputInline from "./ConditionalOutputInline";
+import IfElse from "./IfElse";
+import ImpliedReturn from "./ImpliedReturn";
+import LegacyFunctions from "./LegacyFunctions";
+import TernaryOperator from "./TernaryOperator";
+import VariablesAndConstants from "./VariablesAndConstants";
+import VariableTypes from "./VariableTypes";
+
+export default function Lab3() {
+    return (
+        <div id="wd-lab3">
+            <h3>Lab 3</h3>
+            <VariablesAndConstants/>
+            <VariableTypes/>
+            <BooleanVariables/>
+            <IfElse/>
+            <TernaryOperator/>
+            <ConditionalOutputIfElse/>
+            <ConditionalOutputInline/>
+            <LegacyFunctions/>
+            <ArrowFunctions/>
+            <ImpliedReturn/>
+        </div>
+    );
+}
+  
