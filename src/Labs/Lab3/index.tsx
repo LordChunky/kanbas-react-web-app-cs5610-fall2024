54818c1f30a5d80919fc5015b27972e9808692c7
Quang Gia Nguyen <91767053+LordChunky@users.noreply.github.com>
2024-10-11 22:28:22 -0400

    Finished "2.4.10 Rendering a Data Structure "

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,11 +1,23 @@
+import AddingAndRemovingToFromArrays from "./AddingAndRemovingToFromArrays";
+import ArrayIndexAndLength from "./ArrayIndexAndLength";
 import ArrowFunctions from "./ArrowFunctions";
 import BooleanVariables from "./BooleanVariables";
 import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
 import ConditionalOutputInline from "./ConditionalOutputInline";
+import FilterFunction from "./FilterFunction";
+import FindFunction from "./FindFunction";
+import FindIndex from "./FindIndex";
+import ForLoops from "./ForLoops";
+import House from "./House";
 import IfElse from "./IfElse";
 import ImpliedReturn from "./ImpliedReturn";
+import JsonStringify from "./JsonStringify";
 import LegacyFunctions from "./LegacyFunctions";
+import MapFunction from "./MapFunction";
+import SimpleArrays from "./SimpleArrays";
+import TemplateLiterals from "./TemplateLiterals";
 import TernaryOperator from "./TernaryOperator";
+import TodoList from "./todos/TodoList";
 import VariablesAndConstants from "./VariablesAndConstants";
 import VariableTypes from "./VariableTypes";
 
@@ -23,6 +35,18 @@ export default function Lab3() {
             <LegacyFunctions/>
             <ArrowFunctions/>
             <ImpliedReturn/>
+            <TemplateLiterals/>
+            <SimpleArrays/>
+            <ArrayIndexAndLength/>
+            <AddingAndRemovingToFromArrays/>
+            <ForLoops/>
+            <MapFunction/>
+            <FindFunction/>
+            <FindIndex/>
+            <FilterFunction/>
+            <JsonStringify/>
+            <House/>
+            <TodoList/>
         </div>
     );
 }
