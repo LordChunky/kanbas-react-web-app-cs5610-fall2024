a4fd2ac0d4ed71ab8d80ab478d34d8befc9003b8
Quang Gia Nguyen <91767053+LordChunky@users.noreply.github.com>
2024-10-28 14:13:20 -0400

    Finished section "3.1.3 Editing a Course"

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -30,12 +30,25 @@ import Add from "./Add";
 import Square from "./Square";
 import Highlight from "./Highlight";
 import PathParameters from "./PathParameters";
+import { useSelector } from "react-redux";
+import TodoForm from "../Lab4/ReduxExamples/todos/TodoForm";
 
 export default function Lab3() {
-    console.log('Hello World!');
+    const { todos } = useSelector((state: any) => state.todosReducer);
     return (
         <div id="wd-lab3">
             <h3>Lab 3</h3>
+            {/* Global component */}
+            <TodoForm/>
+            <ul className="list-group">
+                {todos.map((todo: any) => (
+                <li className="list-group-item" key={todo.id}>
+                    {todo.title}
+                </li>
+                ))}
+            </ul>
+            <hr />
+
             <VariablesAndConstants/>
             <VariableTypes/>
             <BooleanVariables/>
--- src/Labs/Lab4/ReduxExamples/todos/TodoForm.tsx
+++ src/Labs/Lab4/ReduxExamples/todos/TodoForm.tsx
@@ -1,28 +1,30 @@
-export default function TodoForm({ todo, setTodo, addTodo, updateTodo }: {
-    todo: { id: string; title: string };
-    setTodo: (todo: { id: string; title: string }) => void;
-    addTodo: (todo: { id: string; title: string }) => void;
-    updateTodo: (todo: { id: string; title: string }) => void;}) {
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { addTodo, updateTodo, setTodo } from "./todosReducer";
+
+export default function TodoForm() {
+    const { todo } = useSelector((state: any) => state.todosReducer);
+    const dispatch = useDispatch();
     return (
         <li className="list-group-item">
-
             <div className="d-flex">
-                <input className="me-auto align-self-center border border-black rounded p-2" defaultValue={todo.title}
-                    onChange={(e) =>
-                    setTodo({ ...todo,
-                        title: e.target.value })
-                    }
-                />
+
+                <input className="me-auto align-self-center border border-black rounded p-2" 
+                    value={todo.title}
+                    onChange={(e) => dispatch(setTodo({ ...todo, title: e.target.value }))} />
+
+
                 <div className="d-flex flex-row-reverse p-2">
-                    <button className="btn btn-success" onClick={() => addTodo(todo)}
+                    <button className="btn btn-success" onClick={() => dispatch(addTodo(todo))}
                             id="wd-add-todo-click">
                         Add
                     </button>
-                    <button className="btn btn-warning me-2" onClick={() => updateTodo(todo)}
+                    <button className="btn btn-warning me-2" onClick={() => dispatch(updateTodo(todo))}
                             id="wd-update-todo-click">
                         Update 
                     </button>
                 </div>
+
             </div>            
 
 
